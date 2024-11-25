e7b44ca8f7d699c4ac92afc484d3d0a65b41fef2
Shehara Kariyawasam <sheharakariyawasam29@gmail.com>
2024-11-25 13:55:04 +0530

    created pages header component router and api folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
 
 export default function App() {
   return (
-    <div className='text-red-700'>
-      app
-    </div>
-  )
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Header/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        
+      </Routes>
+      
+    </BrowserRouter>
+  );
 }
