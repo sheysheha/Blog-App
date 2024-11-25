b7b9cb889e2a348eba270fcc7833acd3c1bf0547
Shehara Kariyawasam <sheharakariyawasam29@gmail.com>
2024-11-25 13:57:36 +0530

    updated features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,24 +4,20 @@ import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
-import ScrollToTop from './components/ScrollToTop';
 import Search from './pages/Search';
 
 export default function App() {
   return (
+    
     <BrowserRouter>
-      <ScrollToTop />
-      <Header />
+    <Header/>
       <Routes>
-        <Header/>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/search' element={<Search />} />
-        
       </Routes>
-      
     </BrowserRouter>
   );
 }
