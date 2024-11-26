529f8a6cd8a9803332230f539762ea8417b30a70
Shehara Kariyawasam <sheharakariyawasam29@gmail.com>
2024-11-26 08:53:53 +0530

    Frontend components and pages

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  apiKey: process.env.VITE_FIREBASE_API_KEY ,
+  authDomain: "blogapp-90b4c.firebaseapp.com",
+  projectId: "blogapp-90b4c",
+  storageBucket: "blogapp-90b4c.firebasestorage.app",
+  messagingSenderId: "550630167228",
+  appId: "1:550630167228:web:9788d75069b34b240a76b9"
 };
 
 // Initialize Firebase
